b7a328dadd028f0c6a3c6dbdc9a7491f62291d0b
AidanTheActualAsian <56741029+AidanTheActualAsian@users.noreply.github.com>
2022-03-16 21:55:11 -0400

    3/16/22 -- commit 2 -- Began coding Bluetooth functionality based on Android Studio documentation. Not sure about the implementation on any of this right now- will need to test. Only implements Bluetooth discoverability. To-Do: Work on Bluetooth connection stuff.

--- app/src/main/java/com/example/bloodpressuremonitor/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/bloodpressuremonitor/ui/gallery/GalleryFragment.java
@@ -1,16 +1,29 @@
 package com.example.bloodpressuremonitor.ui.gallery;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.bloodpressuremonitor.R;
 import com.example.bloodpressuremonitor.databinding.FragmentGalleryBinding;
+
+import java.util.Set;
 
 public class GalleryFragment extends Fragment {
 
@@ -23,6 +36,82 @@ public class GalleryFragment extends Fragment {
 
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        int REQUEST_ENABLE_BT = 0;
+        Button buttonClick = (Button) root.findViewById(R.id.button);
+        TextView tv = (TextView) root.findViewById(R.id.text_gallery);
+
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                if (bluetoothAdapter == null) {
+                    tv.setText("Bluetooth Not Supported");
+                }
+
+                if (!bluetoothAdapter.isEnabled()) {
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                }
+
+                // Lots of permission stuff. Won't be able to do much with this until we know the
+                // specific bluetooth details of the device (e.g. name and MAC address)
+                if (ActivityCompat.checkSelfPermission(root.getContext(), Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+                if (pairedDevices.size() > 0) {
+                    for (BluetoothDevice device : pairedDevices) {
+                        String deviceName = device.getName();
+                        String deviceHardwareAddress = device.getAddress();
+                    }
+                }
+            }
+
+
+            protected void onCreate(Bundle savedInstanceState) {
+                // Register for broadcasts when a device is discovered
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                getActivity().registerReceiver(receiver, filter);
+            }
+
+            // Create a BroadcastReceiver for ACTION_FOUND.
+            private final BroadcastReceiver receiver = new BroadcastReceiver() {
+                public void onReceive(Context context, Intent intent) {
+                    String action = intent.getAction();
+                    if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                        // Discovery found device. Get Bluetooth Device and info.
+                        BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                        if (ActivityCompat.checkSelfPermission(root.getContext(), Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                            // TODO: Consider calling
+                            //    ActivityCompat#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for ActivityCompat#requestPermissions for more details.
+                            return;
+                        }
+                        String deviceName = device.getName();
+                        String deviceHardwareAddress = device.getAddress();
+                    }
+                }
+            };
+
+
+            protected void onDestroy() {
+                // ???? not sure if I need to add anything else?
+                getActivity().unregisterReceiver(receiver);
+            }
+        });
 
         final TextView textView = binding.textGallery;
         galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
